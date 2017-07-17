306a6cf6d06cb1ef2d094f62891b8e5e5cad2779
Philipp Marmet <philipp.marmet@adfinis-sygroup.ch>
2017-07-17 11:12:32 +0200

    init push

---
+++ src/main.ino
@@ -0,0 +1,165 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+#define DHTTYPE DHT22
+#define DHTPIN  2
+
+String client_id        = "sensor_sys_out";
+const char* ssid        = "WIFI";
+const char* password    = "very_secure_p455w0rd";
+const char* mqtt_server = "mqtt-broker.example.com";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+char conv_string[15];
+
+ 
+// Initialize DHT sensor 
+// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
+// you need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// This is for the ESP8266 processor on ESP-01 
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+ 
+float humidity, temp_f, temp_c;  // Values read from sensor
+// Generally, you should use "unsigned long" for variables that hold time
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
+ 
+void setup(void)
+{
+  Serial.begin(115200); // initialize serial connection
+  dht.begin();          // initialize dht sensor
+ 
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("\n\r \n\rWorking to connect");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("DHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start the Pub/Sub client
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+}
+ 
+void loop(void)
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+
+  long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
+
+    gettemperature();       // read sensor
+
+    StaticJsonBuffer<300> JSONbuffer;
+    char JSONmessageBuffer[300];
+    JsonObject& JSONencoder = JSONbuffer.createObject();
+
+    JSONencoder["sensor"]      = client_id;
+    JSONencoder["humidity"]    = (int)humidity;
+    JSONencoder["temperature"] = (float)temp_c;
+
+    JSONencoder.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+
+    client.publish("sysensors/sensor_sys_out/temperature", JSONmessageBuffer);
+  }
+
+} 
+ 
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+ 
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
+    temp_c = dht.readTemperature();         // Read temperature as Fahrenheit
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp_f) || isnan(temp_c)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    // Create a random client ID
+    String clientId = client_id + "-";
+    clientId += String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
