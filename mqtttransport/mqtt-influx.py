37b0e40143d3f15af8e08be424fea21e6373a3a0
Marc Urben <aegnor@mittelerde.ch>
2017-08-11 11:51:18 +0200

    It finally works. Lots of cleanup required

--- mqtttransport/mqtt-influx.py
+++ mqtttransport/mqtt-influx.py
@@ -8,6 +8,9 @@ import traceback
 from influxdb import InfluxDBClient
 import paho.mqtt.client as mqtt
 
+import time
+time.sleep(30)
+
 # Stat counter
 write_counter = 0
 
@@ -43,20 +46,21 @@ def on_message(client, userdata, msg):
     ]
 
     res = influx_client.write_points(json_body)
-    write_counter += 1
-    if write_counter % 1000 == 0:
-        logging.info('Wrote %s sets of data to influxdb (res: %s)' % (write_counter, res))
+#    write_counter += 1
+#    if write_counter % 1000 == 0:
+#        logging.info('Wrote %s sets of data to influxdb (res: %s)' % (write_counter, res))
     
 logging.info('Connecting to influx on %s:%s as %s to db %s' % (if_host, if_port, if_user, if_daba))
 influx_client = InfluxDBClient(host=if_host, port=if_port, username=if_user, password=if_pass, database=if_daba)
+influx_client.create_database(if_daba)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 logging.info('Connecting to MQTT on %s:%s' % (mq_host, mq_port))
-#client.connect(mq_host, mq_port, 60)
-client.connect("mosquitto", 1883, 60)
+client.connect(mq_host, mq_port, 60)
+#client.connect("localhost", 1883, 60)
 
 while True:
     try:
