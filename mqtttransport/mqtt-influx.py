9ccd0945eee10d63070135411fddeaa261ec99e3
Marc Urben <aegnor@mittelerde.ch>
2017-08-11 16:32:39 +0200

    Working on class optimization

--- mqtttransport/mqtt-influx.py
+++ mqtttransport/mqtt-influx.py
@@ -36,6 +36,28 @@ class MqttTransport:
     def on_connect(self, client, userdata, flags, rc):
         client.subscribe("sysensors/+/temperature")
 
+    def setup_mqtt_client(self):
+        if not self.mqtt_client:
+            self.mqtt_client = mqtt.Client()
+            self.mqtt_client.on_connect = self.on_connect
+            self.mqtt_client.on_message = self.on_message
+
+            logging.info('Connecting to MQTT on %s:%s' % (self.mq_host,
+                                                          self.mq_port))
+            self.mqtt_client.connect(self.mq_host, self.mq_port, 60)
+
+    def setup_influx_client(self):
+        logging.info('Connecting to influx on %s:%s as %s to db %s' % (self.if_host,
+                                                                       self.if_port,
+                                                                       self.if_user,
+                                                                       self.if_daba))
+        self.influx_client = InfluxDBClient(host=self.if_host,
+                                            port=self.if_port,
+                                            username=self.if_user,
+                                            password=self.if_pass,
+                                            database=self.if_daba)
+        self.influx_client.create_database(self.if_daba)
+
     def on_message(self, client, userdata, msg):
         json_data = json.loads(msg.payload)
 
@@ -57,28 +79,10 @@ class MqttTransport:
         if self.write_counter % self.pagination == 0:
             logging.info('Wrote %s sets of data to influxdb (res: %s)' % (self.write_counter, res))
 
-    def setup(self):
-        logging.info('Connecting to influx on %s:%s as %s to db %s' % (self.if_host,
-                                                                       self.if_port,
-                                                                       self.if_user,
-                                                                       self.if_daba))
-        self.influx_client = InfluxDBClient(host=self.if_host,
-                                            port=self.if_port,
-                                            username=self.if_user,
-                                            password=self.if_pass,
-                                            database=self.if_daba)
-        self.influx_client.create_database(self.if_daba)
-
-        self.mqtt_client = mqtt.Client()
-        self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = self.on_message
-
-        logging.info('Connecting to MQTT on %s:%s' % (self.mq_host,
-                                                      self.mq_port))
-        self.mqtt_client.connect(self.mq_host, self.mq_port, 60)
-        #client.connect("localhost", 1883, 60)
-
     def run(self):
+        self.setup_mqtt_client()
+        self.setup_influx_client()
+
         while True:
             try:
                 self.mqtt_client.loop_forever()
@@ -87,6 +91,7 @@ class MqttTransport:
                 lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
                 emsg = ''.join('' + line for line in lines)
                 logging.warning('Caught exception in mqtt.Client().loop_forever(): \n%s' % (emsg))
+
 
 if __name__ == "__main__":
     transport = MqttTransport()
